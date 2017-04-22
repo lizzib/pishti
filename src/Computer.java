3c6cadb443c5166a27de1e18ce681f2c577b8804
locksmith999 <trentonrothwell3@gmail.com>
2017-04-22 19:05:23 -0400

    Update Computer.java

--- src/Computer.java
+++ src/Computer.java
@@ -1,6 +1,35 @@
+//Lilith Wroth 4/22/2017
 
 public class Computer extends Player {
     //check if capture possible
 
+    public void doTurn(Deck playingDeck) {
+        for (int x = 0; x < hand.size(); x++)//Analizes what is in hand
+        {
+            if (playingDeck.discardPile.get(0).value == hand.get(x).value && hand.get(x).value != 11) {
+                playingDeck.discardPile.add(hand.get(x));
+                hand.remove(x);
+                return;
+            }
+
+            else if (x == hand.size() - 1) {
+                for (int j = 0; j < hand.size(); j++) {
+                    if (hand.get(j).value != 11) {
+                        playingDeck.discardPile.add(hand.get(j));
+                        hand.remove(j);
+                        return;
+                    }
+                    else if(j == hand.size() - 1 ){
+                        playingDeck.discardPile.add(hand.get(0));
+                        hand.remove(0);
+                        return;
+                    }
+
+                }
+            }
+
+
+        }
+    }
     //play jack last
 }
