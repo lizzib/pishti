0bb9a8cdf1a9cfccaf999aa9e000b5a093ed422c
locksmith999 <trentonrothwell3@gmail.com>
2017-04-22 19:05:43 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 //ELIZABETH BURNSIDE
+//Lilith Wroth 4/22/2017
 
 public class Player {
     int score = 0;
@@ -16,7 +17,7 @@ public class Player {
     public void redraw(Deck playingDeck){
         if(playingDeck.drawPile.size() > 10)
         {
-            for(int i = 0; i < 5; i++)
+            for(int i = 0; i < 4; i++)
                 playingDeck.draw(this);
         }
 
@@ -27,6 +28,48 @@ public class Player {
         {
             captured.add(playingDeck.discardPile.get(i));
             playingDeck.discardPile.remove(i);
+        }
+    }
+
+    public void capture(Deck playingDeck, Player player){
+
+        boolean Captured = playingDeck.discardPile.get(0).value == playingDeck.discardPile.get(1).value;
+
+        //runs anytime a player puts down a card to see if captured
+        if (Captured) {
+            //Checks for Pişti
+            if (playingDeck.discardPile.size() == 2 && playingDeck.discardPile.get(0) == playingDeck.discardPile.get(1)) {
+                player.score += 10;
+
+                //Double Pişti
+                if (playingDeck.discardPile.get(0).value == 11) {
+                    player.score += 10;
+                }
+            }
+            player.captured.addAll(playingDeck.discardPile);
+
+            while (playingDeck.discardPile.size() != 0) {
+
+                //10's, J's, Q's, K's, A's.
+                if (playingDeck.discardPile.get(0).value > 2 || playingDeck.discardPile.get(0).value <= 10) {
+                    score += 1;
+                }
+
+                //diamond 10
+                if (playingDeck.discardPile.get(0).value == 10 && playingDeck.discardPile.get(0).suite == 2) {
+                    score += 2;
+                }
+
+                //2 of clubs
+                if (playingDeck.discardPile.get(0).value == 2 && playingDeck.discardPile.get(0).suite == 3) {
+                    score += 2;
+                }
+
+                //removes card
+                captured.add(playingDeck.discardPile.get(0));
+                playingDeck.discardPile.remove(0);
+            }
+
         }
     }
 
