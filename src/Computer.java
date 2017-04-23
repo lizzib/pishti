93b5e2dfed2fb4f283699bdad385e2eb68a0f876
locksmith999 <trentonrothwell3@gmail.com>
2017-04-23 15:34:58 -0400

    Update Computer.java

--- src/Computer.java
+++ src/Computer.java
@@ -1,18 +1,15 @@
 //Lilith Wroth 4/22/2017
 
 public class Computer extends Player {
-public Card cpuCard;
+    public Card cpuCard;
 
     //check if capture possible
     //Advanced Ai Algorithm
     public int doTurn(Deck playingDeck) {
-        System.out.print("Yes i have done my turn myaster");
         for (int x = 0; x < hand.size(); x++)//Analizes what is in hand
         {
             //Tries to match cards ignoring jacks to get the most out of them
             if (playingDeck.discardPile.get(0).value == hand.get(x).value && hand.get(x).value != 11) {
-                playingDeck.discardPile.add(hand.get(x));
-                capture(playingDeck, this);
                 return x;
             }
 
@@ -24,22 +21,17 @@ public Card cpuCard;
 
                     //ignores jacks
                     if (hand.get(j).value != 11) {
-                        playingDeck.discardPile.add(hand.get(j));
-                        capture(playingDeck, this);
                         return j;
                     }
 
                     //if nothing else left plays a jack
                     else if (j == hand.size() - 1) {
-                        playingDeck.discardPile.add(hand.get(0));
-                        capture(playingDeck, this);
                         return 0;
                     }
 
                 }
             }
         }
-        hand.remove(0);
         return 0;
     }
 }
