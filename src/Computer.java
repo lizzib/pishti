f517850df7899683fd68448fd60e8299c2c20979
lizzib <lizzieburnside@gmail.com>
2017-04-23 00:50:08 -0400

    Update Computer.java

--- src/Computer.java
+++ src/Computer.java
@@ -1,19 +1,19 @@
 //Lilith Wroth 4/22/2017
 
 public class Computer extends Player {
-
+public Card cpuCard;
 
     //check if capture possible
     //Advanced Ai Algorithm
-    public void doTurn(Deck playingDeck, Computer cpu) {
-            for (int x = 0; x < hand.size(); x++)//Analizes what is in hand
+    public int doTurn(Deck playingDeck) {
+        System.out.print("Yes i have done my turn myaster");
+        for (int x = 0; x < hand.size(); x++)//Analizes what is in hand
         {
             //Tries to match cards ignoring jacks to get the most out of them
             if (playingDeck.discardPile.get(0).value == hand.get(x).value && hand.get(x).value != 11) {
                 playingDeck.discardPile.add(hand.get(x));
-                hand.remove(x);
-                capture(playingDeck, cpu);
-                return;
+                capture(playingDeck, this);
+                return x;
             }
 
             //if no matching card
@@ -25,23 +25,21 @@ public class Computer extends Player {
                     //ignores jacks
                     if (hand.get(j).value != 11) {
                         playingDeck.discardPile.add(hand.get(j));
-                        hand.remove(j);
-                        capture(playingDeck, cpu);
-                        return;
+                        capture(playingDeck, this);
+                        return j;
                     }
 
                     //if nothing else left plays a jack
-                    else if(j == hand.size() - 1 ){
+                    else if (j == hand.size() - 1) {
                         playingDeck.discardPile.add(hand.get(0));
-                        hand.remove(0);
-                        capture(playingDeck, cpu);
-                        return;
+                        capture(playingDeck, this);
+                        return 0;
                     }
 
                 }
             }
-
-
         }
+        hand.remove(0);
+        return 0;
     }
 }
