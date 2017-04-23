79b1eab7d4b913afc671aa399413062f191a7d38
locksmith999 <trentonrothwell3@gmail.com>
2017-04-23 19:36:25 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -22,49 +22,4 @@ public class Player {
         }
 
     }
-/*
-    public void capture(Deck playingDeck, Player player){
-
-        boolean Captured = (playingDeck.discardPile.get(0).value == playingDeck.discardPile.get(1).value || playingDeck.discardPile.get(0).value == 11);
-
-        //runs anytime a player puts down a card to see if captured
-        if (Captured) {
-            System.out.println("\n***GOTCHA!!!***\n");
-            //Checks for Pişti
-            if (playingDeck.discardPile.size() == 2 && playingDeck.discardPile.get(0) == playingDeck.discardPile.get(1)) {
-                player.score += 10;
-
-                //Double Pişti
-                if (playingDeck.discardPile.get(0).value == 11) {
-                    player.score += 10;
-                }
-            }
-            
-            while (playingDeck.discardPile.size() != 0) {
-
-                //10's, J's, Q's, K's, A's.
-                if (playingDeck.discardPile.get(0).value > 2 || playingDeck.discardPile.get(0).value <= 10) {
-                    score += 1;
-                }
-
-                //diamond 10
-                if (playingDeck.discardPile.get(0).value == 10 && playingDeck.discardPile.get(0).suite == 2) {
-                    score += 2;
-                }
-
-                //2 of clubs
-                if (playingDeck.discardPile.get(0).value == 2 && playingDeck.discardPile.get(0).suite == 3) {
-                    score += 2;
-                }
-
-                //removes cards from discard and adds them to the captured cards
-                captured.add(playingDeck.discardPile.get(0));
-                playingDeck.discardPile.remove(0);
-            }
-
-        }
-        
-        //Do End Turn
-    }
-    */
 }
